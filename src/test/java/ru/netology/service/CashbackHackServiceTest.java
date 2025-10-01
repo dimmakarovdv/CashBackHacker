ef79901e38379cbe688d9fe080f2e43dd424b220
Neto test <dimmakarovdv89@mail.ru>
2025-10-01 23:09:41 +0500

    gradle updated: added the junit4 branch

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,4 +1,27 @@
 package ru.netology.service;
 
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
 public class CashbackHackServiceTest {
+
+    @org.junit.Test
+    public void testRemain() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 1000;
+        assertEquals(actual, expected);
+    }
+
+    @org.junit.Test
+    public void testRemainWithCashback() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+        int actual = service.remain(amount);
+        int expected = 100;
+        assertEquals(actual, expected);
+    }
 }
+
+
