225bdeffecc04ecd01cdc789abbeb990f7679e99
Neto test <dimmakarovdv89@mail.ru>
2025-10-01 23:27:15 +0500

    gradle updated: added the TestNG branch

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,4 +1,25 @@
 package ru.netology.service;
 
+import org.testng.annotations.Test;
+import static org.testng.Assert.assertEquals;
+
 public class CashbackHackServiceTest {
+
+    @Test
+    public void testRemain() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;  // Исправлено: должно быть 0, а не 1000
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testRemainWithCashback() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+        int actual = service.remain(amount);
+        int expected = 100;
+        assertEquals(actual, expected);
+    }
 }
